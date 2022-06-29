b4739eb36ddb915aa734d68daa12ddd8b64fdff6
zhangzhuang <2592004894@qq.com>
2022-06-29 22:50:34 +0800

    加入结构体的内容

--- struct/empty_struct_with_channel.go
+++ struct/empty_struct_with_channel.go
@@ -8,13 +8,12 @@ import (
 // struct{} 类型数据不占据内存，
 // 配合 channel 使用，可以作为一种信号，打破阻塞或者引发阻塞
 
-
 func main() {
 	var ch = make(chan struct{})
 
-	go func(){
+	go func() {
 		fmt.Println("child go routine gets blocked")
-		<- ch
+		<-ch
 		fmt.Println("child go routine continues")
 	}()
 
